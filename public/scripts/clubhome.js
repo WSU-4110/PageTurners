fd632973c0962308927ad5dbff71309e4ea05d6e
Ben Sanderson <hm9630@wayne.edu>
2024-12-02 19:03:47 -0500

    fixed club home page and lists

--- public/scripts/clubhome.js
+++ public/scripts/clubhome.js
@@ -30,111 +30,214 @@ from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
   const auth = getAuth();
   const db = getFirestore(app);
 
+  document.body.style.visibility = "visible";
   const queryParams = new URLSearchParams(window.location.search);
   let userUID = "";
+  let userEmail = "";
   auth.onAuthStateChanged(function(user){
     if (user) {
-        userUID = user.uid;      
+        userUID = user.uid;
+        userEmail = user.email;    
     }
-  })
+  });
 
-  function createDisPost(docRef)
+  async function isAdmin(userUID, docRef)
   {
-    const li = document.createElement("li")
-    const p1 = document.createElement("p")
-    const p2 = document.createElement("p");
-    const p3 = document.createElement("p");
-    const small = document.createElement("small");
-
-    p1.textContent = docRef.data()["Title"];
-    p1.setAttribute("class","bold underline");
-    p2.textContent = "By: " + docRef.data()["Author"];
-    p2.setAttribute("class","bold")
-    p3.textContent = docRef.data()["Body"];
-    small.textContent = docRef.data()["postDate"].toDate();
-
-    li.appendChild(p1);
-    li.appendChild(p2);
-    li.appendChild(p3);
-    li.appendChild(small);
-
-    return li;
-
-  }
-
-  async function isAdmin(userUID)
-  {
-      const docRef = await getClubdocRefFromQParams(new URLSearchParams(window.location.search));
       if (docRef.data()["ClubAdmins"].includes(userUID))
       {
           return 1;
       }
       return 0;
   }
-
-  async function getClubdocRefFromQParams(queryParams)
-  {
-    let docRef = doc(db,"BookClubs", queryParams.get("id"));
-    const docSnap = await getDoc(docRef);
-    console.log(docSnap.data());
-    return docSnap;
-  }
-
-
-
-
-  async function getEmailFromUID(uid)
-  {
-    const q = query(collection(db, "Users"), where("uid", "==", uid));
-    const qsnap = await getDocs(q);
-
-    return qsnap.docs[0].data()["email"];
-  }
-
-
-
-    const post = document.getElementById("post");
-    post.href = "discussionpost.html?id=" + queryParams.get("id");
+    
   
+  class Clubhomepage{
 
-  let docRef = await getClubdocRefFromQParams(queryParams);
+    constructor()
+    {
+      this.ClubDocSnap = null;
+      this.ClubDocRef = null;
+      this.DiscDocSnap = null;
+      this.ClubName = "";
+      this.ClubDesc = "";
+      this.CurrBook = "";
+      this.CurrReading = "";
+      this.DiscTopic = "";
+      this.Members = [];
+      this.Discussion =[];
+    }
 
-  document.getElementById("clubName").textContent = "Welcome to the \"" + docRef.data()["BookClubName"] + "\" Book Club"
-  document.getElementById("clubDescription").textContent = docRef.data()["clubDescription"]
-  document.getElementById("booktitle").textContent = docRef.data()["clubBook"];
-  document.getElementById("bookdesc").textContent = docRef.data()["clubWeekReading"];
-  document.getElementById("disctopic").textContent = "Current Discussion Topic: " + docRef.data()["discussionTopic"]
-  const parentUL = document.getElementById("members");
+    updatePage()
+    {
+      document.getElementById("clubName").textContent = "Welcome to the \"" + this.ClubName + "\" Book Club";
+      document.getElementById("clubDescription").textContent = this.ClubDesc;
+      document.getElementById("booktitle").textContent = this.CurrBook;
+      document.getElementById("bookdesc").textContent = this.CurrReading;
+      document.getElementById("disctopic").textContent = "Current Discussion Topic: " + this.DiscTopic;
 
+      const memberUL = document.getElementById("members");
+      for (const email of this.Members)
+      {
+        const newLI = document.createElement("li");
+        newLI.textContent = email;
+        memberUL.appendChild(newLI);
+      }
 
-  for (const uid of docRef.data()["ClubUsers"])
-  {
-    const newLI = document.createElement("li");
-    newLI.textContent = await getEmailFromUID(uid);
+      const dicsussionUL = document.getElementById("disc");
 
-    parentUL.appendChild(newLI);
+      for (const post of this.Discussion)
+      {
+        dicsussionUL.appendChild(post);
+      }
+
+      document.body.style.visibility = "visibile";
+    }
+
+    async GenClubDocRefAndSnap()
+    {
+      let docRef = doc(db,"BookClubs", queryParams.get("id"));
+      this.ClubDocRef = docRef;
+      const docSnap = await getDoc(docRef);
+      this.ClubDocSnap = docSnap;
+      const discRef = collection(docRef, "DiscussionPosts");
+      const dq = query(discRef, orderBy("postDate"));
+      const dqsnap = await getDocs(dq);
+      this.DiscDocSnap = dqsnap;
+      return this.ClubDocSnap;
+    }
+
+    async genDiscSnap()
+    {
+      const discRef = collection(this.ClubDocRef, "DiscussionPosts");
+      const dq = query(discRef, orderBy("postDate"));
+      const dqsnap = await getDocs(dq);
+      this.DiscDocSnap = dqsnap;
+    }
+
+    setClubName()
+    {
+      this.ClubName = this.ClubDocSnap.data()["BookClubName"];
+      return this.ClubName;
+    }
+
+    setClubClubDesc()
+    {
+      this.ClubDesc = this.ClubDocSnap.data()["clubDescription"];
+      return this.ClubDesc;
+    }
+
+    setCurrBook()
+    {
+      this.CurrBook = this.ClubDocSnap.data()["clubBook"];
+      return this.CurrBook;
+    }
+
+    setCurrReading()
+    {
+      this.CurrReading = this.ClubDocSnap.data()["clubWeekReading"];
+      return this.CurrReading;
+    }
+
+    setDiscTopic()
+    {
+      this.DiscTopic = this.ClubDocSnap.data()["discussionTopic"];
+      return this.DiscTopic;
+    }
+
+    setMembers()
+    {
+      this.Members = this.ClubDocSnap.data()["ClubUsers"];
+      return this.Members;
+    }
+
+    async convertMembers()
+    {
+      for (let i = 0; i < this.Members.length; i++)
+      {
+        this.Members[i] = await this.getEmailFromUID(this.Members[i]);
+      }
+    }
+
+    setDiscussion()
+    {
+      this.DiscDocSnap.forEach((doc)=>
+      {
+        this.Discussion.push(this.createDisPost(doc));
+      })
+      return this.Discussion;
+    }
+
+    getClubDoc()
+    {
+      return this.ClubDocSnap;
+    }
+
+    async getEmailFromUID(uid)
+    {
+     const q = query(collection(db, "Users"), where("uid", "==", uid));
+     const qsnap = await getDocs(q);
+
+     return qsnap.docs[0].data()["email"];
+    }
+
+    createDisPost(docRef)
+    {
+      const li = document.createElement("li")
+      const p1 = document.createElement("p")
+      const p2 = document.createElement("p");
+      const p3 = document.createElement("p");
+      const small = document.createElement("small");
+
+      p1.textContent = docRef.data()["Title"];
+      p1.setAttribute("class","bold underline");
+      p2.textContent = "By: " + docRef.data()["Author"];
+      p2.setAttribute("class","bold")
+      p3.textContent = docRef.data()["Body"];
+      small.textContent = docRef.data()["postDate"].toDate();
+
+      li.appendChild(p1);
+      li.appendChild(p2);
+      li.appendChild(p3);
+      li.appendChild(small);
+
+      return li;
+
+    }
   }
-  let docRef2 = doc(db,"BookClubs", queryParams.get("id"));
+
+const Homepage = new Clubhomepage();
+
+
+
+await Homepage.GenClubDocRefAndSnap().then(async ()=>
+{
   
-  const discRef = collection(docRef2, "DiscussionPosts");
-  const dq = query(discRef, orderBy("postDate"));
-  const dqsnap = await getDocs(dq);
-
-  let elem;
-
-  dqsnap.forEach((doc)=>{
-
-    elem = createDisPost(doc);
-    document.getElementById("disc").appendChild(elem);
+  Homepage.setClubClubDesc();
+  Homepage.setClubName();
+  Homepage.setCurrBook();
+  Homepage.setCurrReading();
+  Homepage.setDiscTopic();
+  Homepage.setMembers();
+  await Homepage.convertMembers();
+  await Homepage.genDiscSnap().then(()=>
+  {
+    Homepage.setDiscussion();
   })
+  Homepage.updatePage();
+});
 
 
-  if (await isAdmin(userUID) == 1)
-    {
-      document.getElementById("manage").href = "overview.html?id=" + docRef.id;
-    }
-    else
-    {
-      let elem = document.getElementById("manage")
-      elem.remove()
-    }
+if (await isAdmin(userUID, Homepage.getClubDoc()) == 1)
+  {
+    document.getElementById("manage").href = "overview.html?id=" + Homepage.getClubDoc().id;
+  }
+  else
+  {
+    let elem = document.getElementById("manage")
+    elem.remove()
+  }
+
+
+
+
+
